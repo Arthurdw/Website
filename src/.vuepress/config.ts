452ad16519cc7c2a17cc8acecbcdf24061e28829
Arthurdw <dev@arthurdw.com>
2024-12-26 16:43:25 +0100

    feat: added local tracking

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,6 +9,17 @@ export default defineUserConfig({
   description:
     "Arthur De Witte is a passionate full-stack freelance software engineer whom is interested in developing proper, clean and stable code! He likes to utilize Rust, Python, JavaScript/TypeScript for this. Building API's, Websites and scripts are no challenge!",
 
+  head: [
+    [
+      "script",
+      {
+        defer: true,
+        "data-domain": "arthurdw.com",
+        src: "https://analytics.arthurdw.com/js/script.js",
+      },
+    ],
+  ],
+
   theme,
 
   // Enable it with pwa
